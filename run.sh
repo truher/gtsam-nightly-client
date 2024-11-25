8beb881cfb8387a58d7abd379c832ec9545fb356
Joel Truher <joel@truher.org>
2024-11-25 07:19:07 -0800

    add workflow

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@ python3 -m venv demo
 
 demo/bin/python3 -c 'print("created demo venv")'
 
-demo/bin/python3 -m pip install pytest pyparsing
+demo/bin/python3 -m pip install --upgrade pip pytest pyparsing
 
 demo/bin/python3 -m pip install --index-url https://test.pypi.org/simple --extra-index-url https://pypi.org/simple/ truher-gtsam-nightly 
 
