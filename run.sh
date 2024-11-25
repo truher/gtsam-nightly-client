9e2c834090474c3f32f94ee8cc757e897635762c
Joel Truher <joel@truher.org>
2024-11-25 09:00:04 -0800

    run more tests

--- run.sh
+++ run.sh
@@ -1,9 +1,15 @@
+# Create a virtual environment
 python3 -m venv demo
 
-demo/bin/python3 -c 'print("created demo venv")'
-
+# Install tools
 demo/bin/python3 -m pip install --upgrade pip pytest pyparsing
 
+# Checkout gtsam
 demo/bin/python3 -m pip install --index-url https://test.pypi.org/simple --extra-index-url https://pypi.org/simple/ truher-gtsam-nightly 
 
+# Run the local test
 demo/bin/python3 -m pytest
+
+# Run the gtsam installed tests
+demo/bin/python3 -m pytest demo/lib/python3.10/site-packages/gtsam/tests/
+
