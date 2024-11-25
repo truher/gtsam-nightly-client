5e2b6351c2121ae6d1891a21cd9f008005f835f5
Joel Truher <joel@truher.org>
2024-11-25 11:55:29 -0800

    copy gtsam tests here.
    referring to site-packages seems bad.

--- run.sh
+++ run.sh
@@ -7,9 +7,6 @@ demo/bin/python3 -m pip install --upgrade pip pytest pyparsing
 # Checkout gtsam
 demo/bin/python3 -m pip install --index-url https://test.pypi.org/simple --extra-index-url https://pypi.org/simple/ truher-gtsam-nightly 
 
-# Run the local test
+# Run the gtsam tests
 demo/bin/python3 -m pytest
-
-# Run the gtsam installed tests
-demo/bin/python3 -m pytest demo/lib/python3.10/site-packages/gtsam/tests/
 
